ef7a6702b6dc62bbd93f06561e23db4ad8453a2a
Aman Gupta <ag9797191@gmail.com>
2024-09-02 17:52:42 +0530

    changes

--- js/landing pg.js
+++ js/landing pg.js
@@ -23,3 +23,18 @@ let swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entries) => {
+      if (entries.isIntersecting) {
+          entries.target.classList.add("show")
+      } else {
+          entries.target.classList.remove("show")
+      }
+  })
+})
+
+const hiddenElements = document.querySelectorAll(".carousel-caption")
+hiddenElements.forEach((el) =>
+  observer.observe(el)
+)
