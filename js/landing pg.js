673aba0b27dabd5f62410a81651f075a5d7fe941
Aman Gupta <ag9797191@gmail.com>
2024-09-05 21:50:47 +0530

    add

--- js/landing pg.js
+++ js/landing pg.js
@@ -24,70 +24,31 @@ let swiper = new Swiper(".mySwiper", {
   },
 });
 
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entries) => {
+//       if (entries.isIntersecting) {
+//           entries.target.classList.add("show")
+//       } else {
+//           entries.target.classList.remove("show")
+//       }
+//   })
+// })
+
+// const hiddenElements = document.querySelectorAll(".carousel-caption")
+// hiddenElements.forEach((el) =>
+//   observer.observe(el)
+// )
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entries) => {
-      if (entries.isIntersecting) {
-          entries.target.classList.add("show")
-      } else {
-          entries.target.classList.remove("show")
-      }
-  })
-})
-
-const hiddenElements = document.querySelectorAll(".carousel-caption")
-hiddenElements.forEach((el) =>
-  observer.observe(el)
-)
-let items = document.querySelectorAll('.slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-let thumbnails = document.querySelectorAll('.thumbnail .item');
-
-// config param
-let countItem = items.length;
-let itemActive = 0;
-// event next click
-next.onclick = function(){
-    itemActive = itemActive + 1;
-    if(itemActive >= countItem){
-        itemActive = 0;
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
     }
-    showSlider();
-}
-//event prev click
-prev.onclick = function(){
-    itemActive = itemActive - 1;
-    if(itemActive < 0){
-        itemActive = countItem - 1;
-    }
-    showSlider();
-}
-// auto run slider
-let refreshInterval = setInterval(() => {
-    next.click();
-}, 5000)
-function showSlider(){
-    // remove item active old
-    let itemActiveOld = document.querySelector('.slider .list .item.active');
-    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
-    itemActiveOld.classList.remove('active');
-    thumbnailActiveOld.classList.remove('active');
+  });
+});
 
-    // active new item
-    items[itemActive].classList.add('active');
-    thumbnails[itemActive].classList.add('active');
+const hiddenElements = document.querySelectorAll(".hidden");
 
-    // clear auto time run slider
-    clearInterval(refreshInterval);
-    refreshInterval = setInterval(() => {
-        next.click();
-    }, 5000)
-}
-
-// click thumbnail
-thumbnails.forEach((thumbnail, index) => {
-    thumbnail.addEventListener('click', () => {
-        itemActive = index;
-        showSlider();
-    })
-})
+hiddenElements.forEach((el) => observer.observe(el));
